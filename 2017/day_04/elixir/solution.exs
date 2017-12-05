7dd73ee2f080c6ba9b3f01cb2e8bca06ef7d0457
Nicolas Bettenburg <nicbet@Nicolass-MacBook-Pro.local>
2017-12-04 20:12:05 -0500

    Added code comments

--- 2017/day_04/elixir/solution.exs
+++ 2017/day_04/elixir/solution.exs
@@ -1,5 +1,20 @@
+@moduledoc """
+Solutions for Advent of Code 2017 - Day 04
+  High-Entropy Passphrases
+  https://adventofcode.com/2017/day/4
+"""
 defmodule Day4 do
+  @doc """
+  Solution for part 1 of the puzzle.
 
+  The idea is to stream the contents of
+  the given input file line by line,
+  and from this stream of lines filter
+  those lines out that contain duplicate words.
+
+  We use the helper function unique_words?/1 in
+  the filter.
+  """
   def valid_phrases_part_1(source) do
     File.open!(source)
     |> IO.stream(:line)
@@ -7,12 +22,28 @@ defmodule Day4 do
     |> Enum.count
   end
 
-  defp unique_words?(input_string) do
-    words = String.split(input_string)
+  @doc """
+  Determines whether all the words in given sentence are unique.
+  """
+  defp unique_words?(sentence) do
+    words = String.split(sentence)
     uniq_words = Enum.uniq(words)
+    
     Enum.count(words) == Enum.count(uniq_words)
   end
 
+  @doc """
+  Solution for part 2 of the puzzle.
+
+  The idea is to stream the contents of
+  the given input file line-by-line,
+  and filter out all the lines that contain
+  words which are an anagram of another word
+  in the line.
+
+  We use the helper function is_anagram_free?/1
+  in the filter.
+  """
   def valid_phrases_part_2(source) do
     File.open!(source)
     |> IO.stream(:line)
@@ -20,32 +51,26 @@ defmodule Day4 do
     |> Enum.count
   end
 
-  def is_anagram_free?(sentence) do
-    n =
-      String.split(sentence)
-      |> Enum.count
+  @doc """
+  Determines whether a given sentence contains any words where a word is 
+  an anagram of any other word in this sentence.
+  """
+  defp is_anagram_free?(sentence) do
+    # Split the sentence into words and count.
+    n = length(String.split(sentence))
 
-    n_a =
+    # Split the sentence into words, and convert words into their
+    # canonical form (sorted by graphemes). Then obtain the unique
+    # set of canonical words and count.
+    n_a = 
       String.split(sentence)
       |> Enum.map(fn word -> String.graphemes(word) |> Enum.sort end)
       |> Enum.uniq
-     |> Enum.count
+      |> Enum.count
 
+    # If there were any anagrams then the length is not the same
     n == n_a
   end
-
-  def contains_anagrams_for?(word, sentence) do
-    String.split(sentence)
-    |> Enum.filter(&is_anagram?(&1,word))
-    |> Enum.empty? |> Kernel.not
-  end
-
-  def is_anagram?(word1, word2) do
-    left = String.graphemes(word1)
-    right = String.graphemes(word2)
-    Enum.empty?(left -- right)
-  end
-
 end
 
 IO.puts "Part 1"

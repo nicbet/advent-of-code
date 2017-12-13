1f0cd789c5ddf6cbb5b9755cff65dd51c6e25f67
Nicolas Bettenburg <nicbet@users.noreply.github.com>
2017-12-12 20:04:03 -0500

    Finally had time to get through Day_03 Part 2

--- 2017/day_03/go/main.go
+++ 2017/day_03/go/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -15,10 +15,10 @@ func abs(x int) int {
 }
 
 func main() {
-	
+
 	var tgt, err = strconv.Atoi(os.Args[1])
 
-	if (err != nil) {
+	if err != nil {
 		os.Exit(1)
 	}
 	fmt.Println("Finding x,y for Spiral position", tgt)
@@ -28,13 +28,36 @@ func main() {
 	var d = 1
 	var m = 1
 	var index = 1
-	
-	for i:=0; i< 50000; i++ {
-		
 
-		for 2 * x * d < m {
-			fmt.Println(index,":"," (",x,",",y,")")
-			if (index == tgt) {
+	// Initialize grid2d value map
+	var grid2d = map[int]map[int]int{}
+	for x := -1000; x < 1000; x++ {
+		grid2d[x] = map[int]int{}
+		for y := -1000; y < 1000; y++ {
+			grid2d[x][y] = 0
+		}
+	}
+
+	grid2d[0][0] = 1
+
+	for i := 0; i < 50000; i++ {
+
+		for 2*x*d < m {
+			n := grid2d[x][y+1]
+			ne := grid2d[x+1][y+1]
+			nw := grid2d[x-1][y+1]
+			e := grid2d[x+1][y]
+			se := grid2d[x+1][y-1]
+			s := grid2d[x][y-1]
+			sw := grid2d[x-1][y-1]
+			w := grid2d[x-1][y]
+
+			if grid2d[x][y] == 0 {
+				grid2d[x][y] = n + ne + nw + e + se + s + sw + w
+			}
+			fmt.Println(index, ":", " (", x, ",", y, ") =>", grid2d[x][y])
+
+			if index == tgt {
 				var dist = abs(x) + abs(y)
 				fmt.Println("Distance to origin is: ", dist)
 				os.Exit(0)
@@ -42,26 +65,34 @@ func main() {
 			index++
 			x += d
 		}
-		
-		
 
-		for 2 * y * d < m {
-			fmt.Println(index,":"," (",x,",",y,")")
-			if (index == tgt) {
+		for 2*y*d < m {
+			n := grid2d[x][y+1]
+			ne := grid2d[x+1][y+1]
+			nw := grid2d[x-1][y+1]
+			e := grid2d[x+1][y]
+			se := grid2d[x+1][y-1]
+			s := grid2d[x][y-1]
+			sw := grid2d[x-1][y-1]
+			w := grid2d[x-1][y]
+
+			if grid2d[x][y] == 0 {
+				grid2d[x][y] = n + ne + nw + e + se + s + sw + w
+			}
+			fmt.Println(index, ":", " (", x, ",", y, ") =>", grid2d[x][y])
+
+			if index == tgt {
 				var dist = abs(x) + abs(y)
 				fmt.Println("Distance to origin is: ", dist)
 				os.Exit(0)
 			}
-	
+
 			index++
 			y += d
 		}
 
-
 		d *= -1
 		m += 1
-
-		
 
 	}
 }
